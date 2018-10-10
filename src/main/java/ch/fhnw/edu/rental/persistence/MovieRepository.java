b1b9c35a75033fcec73d2c900c4c1e81f5d71b69
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-10 22:42:13 +0200

    Bugfix

--- src/main/java/ch/fhnw/edu/rental/persistence/MovieRepository.java
+++ src/main/java/ch/fhnw/edu/rental/persistence/MovieRepository.java
@@ -11,6 +11,10 @@ import static ch.fhnw.edu.rental.persistence.QueryParameter.with;
 @Repository
 public class MovieRepository extends AbstractJpaRepository<Movie, Long> {
 
+    public MovieRepository() {
+        setClazz(Movie.class);
+    }
+
     public List<Movie> findByTitle(String title) {
         return super.findWithQuery(
             "SELECT m from Movie m where m.title = :title", with("email", title).params());
