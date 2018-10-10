3728b611208f0eda4973a40de5f34ba3b561c20b
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-10 21:13:55 +0200

    Impl JpaUserRepo

--- src/main/java/ch/fhnw/edu/rental/persistence/impl/JpaUserRepository.java
+++ src/main/java/ch/fhnw/edu/rental/persistence/impl/JpaUserRepository.java
@@ -3,71 +3,73 @@ package ch.fhnw.edu.rental.persistence.impl;
 import java.util.List;
 import java.util.Optional;
 
+import ch.fhnw.edu.rental.persistence.AbstractJpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ch.fhnw.edu.rental.model.User;
 import ch.fhnw.edu.rental.persistence.UserRepository;
 
+import static ch.fhnw.edu.rental.persistence.QueryParameter.*;
+
 @Repository
-public class JpaUserRepository implements UserRepository {
+public class JpaUserRepository extends AbstractJpaRepository<User, Long> implements UserRepository {
+
+    public JpaUserRepository() {
+        setClazz(User.class);
+    }
 
     @Override
     public Optional<User> findById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        return Optional.ofNullable(super.findOne(id));
     }
 
     @Override
     public List<User> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        return super.findAll();
     }
 
     @Override
     public User save(User t) {
-        // TODO Auto-generated method stub
-        return null;
+        return super.update(t);
     }
 
     @Override
     public void deleteById(Long id) {
-        // TODO Auto-generated method stub
+        super.deleteById(id);
 
     }
 
     @Override
     public void delete(User entity) {
-        // TODO Auto-generated method stub
-
+        super.delete(entity);
     }
 
     @Override
     public boolean existsById(Long id) {
-        // TODO Auto-generated method stub
-        return false;
+        return findById(id).isPresent();
     }
 
     @Override
     public long count() {
-        // TODO Auto-generated method stub
-        return 0;
+        return super.count();
     }
 
     @Override
     public List<User> findByLastName(String lastName) {
-        // TODO Auto-generated method stub
-        return null;
+        return super.findWithNamedQuery(
+            "SELECT u FROM User u where u.lastName = :lastName", with("lastName", lastName).params());
     }
 
     @Override
     public List<User> findByFirstName(String firstName) {
-        // TODO Auto-generated method stub
-        return null;
+        return super.findWithNamedQuery(
+            "SELECT u FROM User u where u.firstName = :firstName", with("firstName", firstName).params());
     }
 
     @Override
     public List<User> findByEmail(String email) {
-        // TODO Auto-generated method stub
-        return null;
+        return super.findWithNamedQuery(
+            "SELECT u FROM User u where u.email = :email", with("email", email).params());
+
     }
 }
