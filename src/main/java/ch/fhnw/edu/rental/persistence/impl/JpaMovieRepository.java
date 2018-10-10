872f313fd740bf19526dbff47d8d48bcfc0ec9d1
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-10 13:26:13 +0200

    INit

---
+++ src/main/java/ch/fhnw/edu/rental/persistence/impl/JpaMovieRepository.java
@@ -0,0 +1,64 @@
+package ch.fhnw.edu.rental.persistence.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import ch.fhnw.edu.rental.model.Movie;
+import ch.fhnw.edu.rental.persistence.MovieRepository;
+
+@Repository
+public class JpaMovieRepository implements MovieRepository {
+
+	@Override
+	public Optional<Movie> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Movie> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Movie save(Movie t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Movie entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Movie> findByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
---
+++ src/main/java/ch/fhnw/edu/rental/persistence/impl/JpaRentalRepository.java
@@ -0,0 +1,62 @@
+package ch.fhnw.edu.rental.persistence.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import ch.fhnw.edu.rental.model.Rental;
+import ch.fhnw.edu.rental.model.User;
+import ch.fhnw.edu.rental.persistence.RentalRepository;
+
+@Repository
+public class JpaRentalRepository implements RentalRepository {
+
+	@Override
+	public Optional<Rental> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Rental> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rental save(Rental t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Rental entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Rental> findByUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
