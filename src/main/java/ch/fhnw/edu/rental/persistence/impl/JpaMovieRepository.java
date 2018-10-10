c9dbd3808acacc6dfe9a4c8f9f068e97c1f40705
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-10-10 20:28:34 +0200

    Impl JpaMovieRepo

--- src/main/java/ch/fhnw/edu/rental/persistence/impl/JpaMovieRepository.java
+++ src/main/java/ch/fhnw/edu/rental/persistence/impl/JpaMovieRepository.java
@@ -8,56 +8,63 @@ import org.springframework.stereotype.Repository;
 import ch.fhnw.edu.rental.model.Movie;
 import ch.fhnw.edu.rental.persistence.MovieRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 @Repository
 public class JpaMovieRepository implements MovieRepository {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Override
     public Optional<Movie> findById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        Movie movie = em.find(Movie.class, id);
+        return Optional.ofNullable(movie);
     }
 
     @Override
     public List<Movie> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        return em.createQuery("SELECT m from Movie m", Movie.class).getResultList();
     }
 
     @Override
     public Movie save(Movie t) {
-        // TODO Auto-generated method stub
-        return null;
+        return em.merge(t);
     }
 
     @Override
     public void deleteById(Long id) {
-        // TODO Auto-generated method stub
-
+        Movie movie = findById(id).orElseThrow(() -> new IllegalArgumentException("Can't delete Movie with id =) null"));
+        this.delete(movie);
     }
 
     @Override
     public void delete(Movie entity) {
-        // TODO Auto-generated method stub
-
+        em.remove(entity);
     }
 
     @Override
     public boolean existsById(Long id) {
-        // TODO Auto-generated method stub
-        return false;
+        if (id == null) {
+            return false;
+        }
+        return em.find(Movie.class, id) != null;
     }
 
     @Override
     public long count() {
-        // TODO Auto-generated method stub
-        return 0;
+        TypedQuery<Long> query = em.createQuery("select count(m) from Movie m", Long.class);
+        return query.getSingleResult();
     }
 
     @Override
     public List<Movie> findByTitle(String title) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        TypedQuery<Movie> query = em.createQuery("SELECT m from Movie m where m.title = :title", Movie.class);
+        query.setParameter("title", title);
 
+        return query.getResultList();
+    }
 
 }
